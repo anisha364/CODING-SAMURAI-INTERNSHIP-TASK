eb6b6702dda4f454df68170e173faf157707b313
Anisha <anishajeev@gmail.com>
2026-01-04 20:41:53 +0530

    Add files via upload

---
+++ TASK-2/Number_Guessing.py
@@ -0,0 +1,26 @@
+import random
+
+number = random.randint(1, 10)
+attempts = 0
+
+print("🎯 Welcome to Number Guessing Game!")
+print("Guess a number between 1 and 10")
+
+while True:
+    guess = input("Enter your guess: ")
+
+    if not guess.isdigit():
+        print("Please enter a valid number.")
+        continue
+
+    guess = int(guess)
+    attempts += 1
+
+    if guess == number:
+        print("🎉 Congratulations! You guessed the correct number.")
+        print("Total attempts:", attempts)
+        break
+    elif guess > number:
+        print("Too High! Try again.")
+    else:
+        print("Too Low! Try again.")
