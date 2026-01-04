55c8393303c10a4d9138452829a08224d904c779
Anisha <anishajeev@gmail.com>
2026-01-04 20:23:43 +0530

    Update Simple Calculator.py

--- TASK-1/Simple Calculator.py
+++ TASK-1/Simple Calculator.py
@@ -1,1 +1,24 @@
-t
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print("Result:", num1 + num2)
+elif choice == 2:
+    print("Result:", num1 - num2)
+elif choice == 3:
+    print("Result:", num1 * num2)
+elif choice == 4:
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Division by zero not allowed")
+else:
+    print("Invalid choice")
+
